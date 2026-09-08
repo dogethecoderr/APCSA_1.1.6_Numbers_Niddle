7c5dcc840c830450c48d13691826effbc5b5e243
Akhil Muthyala <akhil.reddym26@gmail.com>
2026-09-08 10:28:30 -0700

    9/8/26

---
+++ NumberRiddles.java
@@ -0,0 +1,73 @@
+public class NumberRiddles {
+    public static void main (String[]args){
+        // starting number you choose
+        int test1 = 5; // positive integer
+        int test2 = -5; // negative integer
+        int test3 = 0;  // zero
+        int test4 = 1; // One
+        double test5 = 5.0;  //Positive double
+        double test6 = -5.5; //Negative double   
+
+        
+        double process1 = test1;
+        // Algorithms to process the numbers
+        process1 = process1 * 2; // double the starting number
+        System.out.println("Double Starting Number: " + process1);
+        process1 += 6;  // add 6 to the result
+        System.out.println("Add 6: " + process1);
+        process1 /= 2; // divide the result by 2
+        System.out.println("Divide by 2: " + process1);
+        System.out.println("Subtract number you started with: " +  (process1 - test1));
+
+        double process2 = test2;
+        // Algorithms to process the numbers
+        process2 = process2 * 2; // double the starting number
+        System.out.println("Double Starting Number: " + process2);
+        process2 += 6;  // add 6 to the result
+        System.out.println("Add 6: " + process2);
+        process2 /= 2; // divide the result by 2
+        System.out.println("Divide by 2: " + process2);
+        System.out.println("Subtract number you started with: " +  (process2 - test2)); // subtract the starting number
+
+        double process3 = test3;
+        // Algorithms to process the numbers
+        process3 *= 2; // double the starting number
+        System.out.println("Double Starting Number: " + process3);
+        process3 += 6;  // add 6 to the result
+        System.out.println("Add 6: " + process3);
+        process3 /= 2; // divide the result by 2
+        System.out.println("Divide by 2: " + process3);
+        System.out.println("Subtract number you started with: " +  (process3 - test3));
+
+        double process4 = test4;
+        // Algorithms to process the numbers
+        process4 *= 2; // double the starting number
+        System.out.println("Double Starting Number: " + process4);
+        process4 += 6;  // add 6 to the result
+        System.out.println("Add 6: " + process4);
+        process4 /= 2; // divide the result by 2
+        System.out.println("Divide by 2: " + process4);
+        System.out.println("Subtract number you started with: " +  (process4 - test4));
+
+        double process5 = test5;
+        // Algorithms to process the numbers
+        process5 = process5 * 2; // double the starting number
+        System.out.println("Double Starting Number: " + process5);
+        process5 += 6;  // add 6 to the result
+        System.out.println("Add 6: " + process5);
+        process5 /= 2; // divide the result by 2
+        System.out.println("Divide by 2: " + process5);
+        System.out.println("Subtract number you started with: " +  (process5 - test5));
+
+        double process6 = test6;
+        // Algorithms to process the numbers
+        process6 = process6 * 2; // double the starting number
+        System.out.println("Double Starting Number: " + process6);
+        process6 += 6;  // add 6 to the result
+        System.out.println("Add 6: " + process6);
+        process6 /= 2; // divide the result by 2
+        System.out.println("Divide by 2: " + process6);
+        System.out.println("Subtract number you started with: " +  (process6 - test6));
+        
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # APCSA_1.1.6_Numbers_Niddle
 Akhil, Guru, Nam, Aedan
+
+Contributions
+-------------
+Guru:
+    Did class defintion header and main method
+    Did Algorithms part 1-3
+
+
+
+
+Nam: 
+    Google Doc Project Documentation, Debugging, 
+
+
+
+
+Aedan:
+    Added all Comments
+
+
+Akhil: 
+    Defined all variables (starting), fixed string literal error, 
+    Did
+
+
+
+
+-------------
