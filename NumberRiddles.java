adab6decd240a0dfa85e1ebf671eb1236b47f7af
NamLeAtDUSD <le0620@mydusd.org>
2026-09-10 08:56:24 -0700

    Update NumberRiddles.java

--- NumberRiddles.java
+++ NumberRiddles.java
@@ -14,7 +14,7 @@ public class NumberRiddles {
 
         double process0 = startingNumber;
         // Algorithms to process the numbers
-        System.out.print("Starting Number: " + startingNumber);
+        System.out.print("Starting Number: " + startingNumber + "\n");
         process0 = process0 * 2; // double the starting number
         System.out.println("Double Starting Number: " + process0);
         process0 += 6;  // add 6 to the result
@@ -26,7 +26,7 @@ public class NumberRiddles {
         
         double process1 = test1;
         // Algorithms to process the numbers
-        System.out.println("Starting Number: " + test1);
+        System.out.print("Starting Number: " + test1 + "\n");
         process1 = process1 * 2; // double the starting number
         System.out.println("Double Starting Number: " + process1);
         process1 += 6;  // add 6 to the result
@@ -37,7 +37,7 @@ public class NumberRiddles {
 
         double process2 = test2;
         // Algorithms to process the numbers
-        System.out.println("Starting Number: " + test2);
+        System.out.print("Starting Number: " + test2 + "\n");
         process2 = process2 * 2; // double the starting number
         System.out.println("Double Starting Number: " + process2);
         process2 += 6;  // add 6 to the result
@@ -48,7 +48,7 @@ public class NumberRiddles {
 
         double process3 = test3;
         // Algorithms to process the numbers
-        System.out.println("Starting Number: " + test3);
+        System.out.print("Starting Number: " + test3 + "\n");
         process3 *= 2; // double the starting number
         System.out.println("Double Starting Number: " + process3);
         process3 += 6;  // add 6 to the result
@@ -59,7 +59,7 @@ public class NumberRiddles {
 
         double process4 = test4;
         // Algorithms to process the numbers
-        System.out.println("Starting Number: " + test4);
+        System.out.print("Starting Number: " + test4 + "\n");
         process4 *= 2; // double the starting number
         System.out.println("Double Starting Number: " + process4);
         process4 += 6;  // add 6 to the result
@@ -70,7 +70,7 @@ public class NumberRiddles {
 
         double process5 = test5;
         // Algorithms to process the numbers
-        System.out.println("Starting Number: " + test5);
+        System.out.print("Starting Number: " + test5 + "\n");
         process5 = process5 * 2; // double the starting number
         System.out.println("Double Starting Number: " + process5);
         process5 += 6;  // add 6 to the result
@@ -81,7 +81,7 @@ public class NumberRiddles {
 
         double process6 = test6;
         // Algorithms to process the numbers
-        System.out.println("Starting Number: " + test6);
+        System.out.print("Starting Number: " + test6 + "\n");
         process6 = process6 * 2; // double the starting number
         System.out.println("Double Starting Number: " + process6);
         process6 += 6;  // add 6 to the result
